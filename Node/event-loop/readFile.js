f51210175bdccc43841bacf6824a59006a6a4e50
Rajal7777 <suwalrajal57@gmail.com>
2026-04-28 23:47:42 +0900

    node read & write 'fs' obj

---
+++ Node/event-loop/readFile.js
@@ -0,0 +1,15 @@
+const { log } = require("console");
+const { readFile, writeFile } = require("fs");
+
+console.log("first test");
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("completed first task");
+});
+
+console.log('finish line')
