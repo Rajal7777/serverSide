864ad736e3dd36081c9fa67908f01f129b8ca4b6
Rajal7777 <suwalrajal57@gmail.com>
2026-04-30 11:08:05 +0900

    added some comments

--- express/express-course/app.js
+++ express/express-course/app.js
@@ -5,13 +5,16 @@ const app = express();
 
 //express automatically pass the parameter -> (req, res, next) from app.get
 const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
+  const method = req.method; //HTTP method like GET, POST, PUT, DELETE
+  const url = req.url;   // from req object we get the value url like '/about'
   const time = new Date().getFullYear();
   console.log("method:", method, "url:", url, "time:", time); //default GET/ '/'2026
-  next(); // this will return to the app.get the next process
+  next(); // next() tells express i am done you can move to the next middleware or route handler
 };
 
+
+// '/' runs 1st then the logger runs-> next() -> then res.send()
+//logger with out () is the value{reference} which will be called by the express later when we get the req.
 app.get("/", logger, (req, res) => {
   res.send("Home");
 });
