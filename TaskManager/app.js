5dfefa833a86a17a48e84cec1678a2cc57b68411
Rajal7777 <suwalrajal57@gmail.com>
2026-06-07 12:43:37 +0900

    custom error handler for next()

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 //to make our express app understand json data coming in from the client, we need to use this middleware. It parses incoming JSON requests and puts the parsed data in req.body.
@@ -11,10 +12,10 @@ app.use(express.json());
 // Serve static files from the "public" directory
 app.use(express.static("./public"));
 
-
 //middleware
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const Port = 4000;
 // console.log(process.env.MONGO_URI);
